ecddba4593a0348ba2d2eec6c1574017caea9eea
Jerry Tong <zihangtong@gmail.com>
2022-01-28 15:30:22 -0500

    Cleanup

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,14 @@
 puts "🌱 Seeding folders..."
 f1 = Folder.create(id: UUID.new.generate, name: "Artists")
 f2 = Folder.create(id: UUID.new.generate, name: "Ruby")
-f3 = Folder.create(id: UUID.new.generate, name: "React")
+f3 = Folder.create(id: UUID.new.generate, name: "JavaScript")
 
 
 puts "🌱 Seeding documents..."
 n1 = Note.create(id: UUID.new.generate, title: Faker::Artist.name, body: "Is he one of the ninja turtles?", folder: f1)
-n2 = Note.create(id: UUID.new.generate, title: Faker::Artist.name, body: "Art stuff", folder: f1)
+n2 = Note.create(id: UUID.new.generate, title: "Frank Sinatra", body: "Jokes", folder: f1)
 n3 = Note.create(id: UUID.new.generate, title: "ActiveRecord", body: "Made with Sinatra ActiveRecord", folder: f2)
-n4 = Note.create(id: UUID.new.generate, title: "JavaScript", body: "Made with React", folder: f3)
+n4 = Note.create(id: UUID.new.generate, title: "React", body: "Made with React JS", folder: f3)
 
 puts "🌱 Seeding tags..."
 t1 = Tag.create(id: UUID.new.generate, name: "ruby")
@@ -19,10 +19,10 @@ t5 = Tag.create(id: UUID.new.generate, name: "sinatra")
 
 puts "Building Note-Tag relations..."
 r1 = NoteTag.create(note: n1, tag: t3)
-r2 = NoteTag.create(note: n1, tag: t5)
-r3 = NoteTag.create(note: n2, tag: t3)
-r4 = NoteTag.create(note: n3, tag: t2)
-r5 = NoteTag.create(note: n3, tag: t1)
+r2 = NoteTag.create(note: n2, tag: t3)
+r3 = NoteTag.create(note: n2, tag: t5)
+r4 = NoteTag.create(note: n3, tag: t1)
+r5 = NoteTag.create(note: n3, tag: t2)
 r6 = NoteTag.create(note: n3, tag: t5)
 r7 = NoteTag.create(note: n4, tag: t4)
 
